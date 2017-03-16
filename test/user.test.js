2be2e770fb519e7080d0481076c3d8272449a840
Trevor Kimenye <kimenye@gmail.com>
2017-03-16 17:57:04 +0300

    Fixed creation tests

--- test/user.test.js
+++ test/user.test.js
@@ -5,21 +5,32 @@ require ('sinon-mongoose')
 
 const User = require('../lib/user.js')
 const Request  = require('../lib/request.js')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
 
 describe('It can handle users', () => {
 
+  const req = { body: { notification_type: 'MessageReceived', text: 'Hi', name: 'Lily', external_contact_id: 'contactId', id: 'messageId', account_type: 'MessengerV2', account: '1234567890' } }
+  const request = new Request(req)
+
+  const UserMock = sinon.mock(User);
+
   it('Should the contact if it exists', () => {
-
-    const req = { body: { notification_type: 'MessageReceived', text: 'Hi', name: 'Lily', external_contact_id: 'contactId', id: 'messageId', account_type: 'MessengerV2', account: '1234567890' } }
-    const request = new Request(req)
-
-    const UserMock = sinon.mock(User);
     UserMock.expects('findOne').withArgs({ contactId: request.contactId, source: request.source, accountType: request.accountType }).yields(null, { contactId: request.contactId })
-
     User.fromRequest(request)
       .then((user) => {
         expect(user.contactId).to.equal(request.contactId)
       })
-      
+  })
+
+  it('Should save the contact if it does not exist', () => {
+    UserMock.expects('findOne').withArgs({ contactId: request.contactId, source: request.source, accountType: request.accountType }).yields(null, null)
+    User.fromRequest(request)
+      .then((user) => {
+        expect(user.contactId).to.equal(request.contactId)
+        expect(user.state).to.equal('new')
+        expect(user.metadata).to.equal(null)
+      })
   })
 })

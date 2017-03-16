60e522da98d272d2097d0325aad0b5a1a7b3828c
Trevor Kimenye <kimenye@gmail.com>
2017-03-16 15:16:25 +0300

    Added user model

---
+++ user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserSchema = Schema({
+  externalId: String,
+  source: String,
+  name: String,
+  state: String,
+  metadata: String
+})
+
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports UserModel

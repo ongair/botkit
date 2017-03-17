904621e02106bb934e5b0693a67a9e89481a4876
Trevor Kimenye <kimenye@gmail.com>
2017-03-17 13:21:56 +0300

    1.0.6

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const User = require('./lib/user.js')
 const Request = require('./lib/request.js')
+const Wizard = require('./lib/wizard.js')
+const Step = require('./lib/step.js')
+const Phrase = require('./lib/phrase.js')
+const { EntryStep } = require('./lib/step_type.js')
 
 module.exports = {
   User: User,
-  Request: Request
+  Request: Request,
+  Wizard: Wizard,
+  Step: Step,
+  EntryStep: EntryStep,
+  Phrase: Phrase
 }
